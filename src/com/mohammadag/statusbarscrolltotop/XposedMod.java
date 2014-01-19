636305777a06c8f9b708eef6d8e9cdd61c0944f4
Dzakus <Dzakus13@gmail.com>
2014-01-19 19:36:53 +0100

    Rewrite 33% of module
    * Add WebView support
    * Add HorizontalScrollView support
    * Remove duplicate code
    * Register receiver in another place
    * Code cleanup
    
    ScrollView/HorizontalScrollView not tested
    Need deep test before accept.

--- src/com/mohammadag/statusbarscrolltotop/XposedMod.java
+++ src/com/mohammadag/statusbarscrolltotop/XposedMod.java
@@ -3,18 +3,20 @@ package com.mohammadag.statusbarscrolltotop;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Rect;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ScrollView;
+import android.webkit.WebView;
+import android.widget.HorizontalScrollView;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
@@ -24,99 +26,101 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedMod implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 	/* My name's here so we don't conflict with other fields, deal with it :p */
-	private static final String KEY_RECEIVERS = "mMohammadAG_scrolToTopReceivers";
+	private static final String KEY_RECEIVER = "mMohammadAG_scrolToTopReceiver";
 
 	/* You can trigger this with any app that can fire intents! */
 	private static final String INTENT_SCROLL_TO_TOP = "com.mohammadag.statusbarscrolltotop.SCROLL_TO_TOP";
 
-	/* We get a MotionEvent when the status bar is tapped, we need to know if it's a click or a drag */
+	/*
+	 * We get a MotionEvent when the status bar is tapped, we need to know if
+	 * it's a click or a drag
+	 */
 	private float mDownX;
 	private float mDownY;
 	private final float SCROLL_THRESHOLD = 10;
 	private boolean mIsClick;
+	private static final Class<?>[] HAPPY_CLASS = new Class[] { 
+		AbsListView.class, 
+		WebView.class, 
+		ScrollView.class,
+		HorizontalScrollView.class };
 
 	/* Some sort of crappy IPC */
-	class ScrollViewReceiver extends BroadcastReceiver {
-		private ScrollView mScrollView;
-		public ScrollViewReceiver(ScrollView view) {
-			mScrollView = view;
+	class ScrollReceiver extends BroadcastReceiver {
+		private ViewGroup mViewGroup;
+
+		public ScrollReceiver(ViewGroup view) {
+			mViewGroup = view;
 		}
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			if (isViewInViewBounds(getContentViewFromContext(mScrollView.getContext()), mScrollView))
-				mScrollView.smoothScrollTo(0, 0);
+			if (isViewInViewBounds(getContentViewFromContext(mViewGroup.getContext()), mViewGroup)) {
+				if (mViewGroup instanceof ScrollView) {
+					((ScrollView) mViewGroup).smoothScrollTo(0, 0);
+				} else if (mViewGroup instanceof AbsListView) {
+					((AbsListView) mViewGroup).smoothScrollToPosition(0);
+				} else if (mViewGroup instanceof WebView) {
+					((WebView) mViewGroup).scrollTo(0, 0);
+				} else if (mViewGroup instanceof HorizontalScrollView) {
+					((HorizontalScrollView) mViewGroup).scrollTo(0, 0);
+				}
+			}
 		}
 	};
 
-	class AbsListViewReceiver extends BroadcastReceiver {
-		private AbsListView mListView;
-		public AbsListViewReceiver(AbsListView view) {
-			mListView = view;
-		}
-
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			if (isViewInViewBounds(getContentViewFromContext(mListView.getContext()), mListView))
-				mListView.smoothScrollToPosition(0);
-		}
-	}
-
 	@Override
 	public void initZygote(StartupParam startupParam) throws Throwable {
-		/* AbsListView, it's one instance of a scroller */
-		findAndHookMethod(AbsListView.class, "initAbsListView", new XC_MethodHook() {
+
+		// TODO Move to const
+		final XC_MethodHook registerHook = new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				AbsListView view = (AbsListView) param.thisObject;
+				ViewGroup view = (ViewGroup) param.thisObject;
 				if (!(view.getContext() instanceof Activity))
 					return;
-				Activity activity = (Activity) view.getContext();		
-				AbsListViewReceiver receiver = new AbsListViewReceiver(view);
-				addReceiverToActivity(activity, receiver);
+				Activity activity = (Activity) view.getContext();
+				BroadcastReceiver receiver = (BroadcastReceiver) XposedHelpers.getAdditionalInstanceField(view,
+						KEY_RECEIVER);
+				if (receiver == null){
+					receiver = new ScrollReceiver(view);
+					XposedHelpers.setAdditionalInstanceField(view, KEY_RECEIVER, receiver);
+				}
 				activity.registerReceiver(receiver, new IntentFilter(INTENT_SCROLL_TO_TOP));
 			}
-		});
+		};
+
+		final XC_MethodHook unregisterHook = new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				ViewGroup view = (ViewGroup) param.thisObject;
+				if (!(view.getContext() instanceof Activity))
+					return;
+				Activity activity = (Activity) view.getContext();
+				BroadcastReceiver receiver = (BroadcastReceiver) XposedHelpers.getAdditionalInstanceField(view,
+						KEY_RECEIVER);
+				if (receiver == null)
+					return;
+				activity.unregisterReceiver(receiver);
+			}
+		};
+
+		for (Class<?> clazz : HAPPY_CLASS) {
+			if (clazz == ScrollView.class) {
+				XposedBridge.hookAllConstructors(clazz, registerHook);
+			} else {
+				findAndHookMethod(clazz, "onAttachedToWindow", registerHook);
+			}
+			findAndHookMethod(clazz, "onDetachedFromWindow", unregisterHook);
+		}
 
 		/* Another one */
-		findAndHookMethod(ScrollView.class, "initScrollView", new XC_MethodHook() {
-			@Override
-			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				ScrollView view = (ScrollView) param.thisObject;
-				if (!(view.getContext() instanceof Activity))
-					return;
-				Activity activity = (Activity) view.getContext();		
-				ScrollViewReceiver receiver = new ScrollViewReceiver(view);
-				addReceiverToActivity(activity, receiver);
-				activity.registerReceiver(receiver, new IntentFilter(INTENT_SCROLL_TO_TOP));
-			}
-		});
 
-		/* FYI, there are some manufacturer specific ones, like Samsung's TouchWiz ones.
-		 * I'll look into those later on...
+		/*
+		 * FYI, there are some manufacturer specific ones, like Samsung's
+		 * TouchWiz ones. I'll look into those later on...
 		 */
 
-		/* We need to register and unregister receivers in onPause and onResume 
-		 * otherwise, Android bitches about it (for good (memory) reason(s) probably). We do that
-		 * by keeping an ArrayList of BroadcastReceivers in all activities. There might be a better
-		 * way...
-		 */
-		Class<?> ActivityClass = findClass("android.app.Activity", null);
-		findAndHookMethod(ActivityClass, "onResume", new XC_MethodHook() {
-			@Override
-			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				Activity activity = (Activity) param.thisObject;
-				resumeBroadcastReceivers(activity);
-			}
-		});
-
-		findAndHookMethod(ActivityClass, "onPause", new XC_MethodHook() {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				Activity activity = (Activity) param.thisObject;
-				pauseBroadcastReceivers(activity);
-			}
-		});
 	}
 
 	@Override
@@ -142,9 +146,12 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookZygoteInit 
 				case MotionEvent.ACTION_UP:
 					if (mIsClick) {
 						try {
-							/* Get NotificationPanelView instance, it subclasses PanelView */
-							Object notificationPanelView =
-									XposedHelpers.getObjectField(param.thisObject, "mNotificationPanel");
+							/*
+							 * Get NotificationPanelView instance, it subclasses
+							 * PanelView
+							 */
+							Object notificationPanelView = XposedHelpers.getObjectField(param.thisObject,
+									"mNotificationPanel");
 
 							float expandedFraction = (Float) XposedHelpers.callMethod(notificationPanelView,
 									"getExpandedFraction");
@@ -152,15 +159,23 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookZygoteInit 
 							if (expandedFraction < 0.1)
 								view.getContext().sendBroadcast(new Intent(INTENT_SCROLL_TO_TOP));
 						} catch (Throwable t) {
-							XposedBridge.log("StatusBarScrollToTop: Unable to determine expanded fraction: " + t.getMessage());
+							XposedBridge.log("StatusBarScrollToTop: Unable to determine expanded fraction: "
+									+ t.getMessage());
 							t.printStackTrace();
 							view.getContext().sendBroadcast(new Intent(INTENT_SCROLL_TO_TOP));
 						}
 					}
 					break;
 				case MotionEvent.ACTION_MOVE:
-					if (mIsClick && (Math.abs(mDownX - ev.getX()) > SCROLL_THRESHOLD || Math.abs(mDownY - ev.getY()) > SCROLL_THRESHOLD)) {
-						mIsClick = false;
+					if (mIsClick) {
+						float x = ev.getX();
+						float y = ev.getY();
+						boolean xMoved = Math.abs(mDownX - x) > SCROLL_THRESHOLD;
+						boolean yMoved = Math.abs(mDownY - y) > SCROLL_THRESHOLD;
+
+						if (xMoved || yMoved) {
+							mIsClick = false;
+						}
 					}
 					break;
 				default:
@@ -170,47 +185,10 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookZygoteInit 
 		});
 	}
 
-	/* Helpers so the code looks less like shit */
-	private static void addReceiverToActivity(Activity activity, BroadcastReceiver receiver) {
-		ArrayList<BroadcastReceiver> receivers = getReceiversForActivity(activity);
-		receivers.add(receiver);
-		XposedHelpers.setAdditionalInstanceField(activity, KEY_RECEIVERS, receivers);
-	}
-
-	@SuppressWarnings("unchecked")
-	private static ArrayList<BroadcastReceiver> getReceiversForActivity(Activity activity) {
-		ArrayList<BroadcastReceiver> receivers = null;
-		try {
-			receivers = (ArrayList<BroadcastReceiver>) XposedHelpers.getAdditionalInstanceField(activity,
-					KEY_RECEIVERS);
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-
-		if (receivers == null) {
-			receivers = new ArrayList<BroadcastReceiver>();
-		}
-
-		return receivers;
-	}
-
-	private static void resumeBroadcastReceivers(Activity activity) {
-		ArrayList<BroadcastReceiver> receivers = getReceiversForActivity(activity);
-		for (BroadcastReceiver receiver : receivers) {
-			activity.registerReceiver(receiver, new IntentFilter(INTENT_SCROLL_TO_TOP));
-		}
-	}
-
-	private static void pauseBroadcastReceivers(Activity activity) {
-		ArrayList<BroadcastReceiver> receivers = getReceiversForActivity(activity);
-		for (BroadcastReceiver receiver : receivers) {
-			activity.unregisterReceiver(receiver);
-		}
-	}
-
-	/* Check if the View is visible to the user, i.e on screen.
-	 * We do this since in tabbed interfaces, we can cause a scrollbar that's off screen
-	 * to go to the top as well as the visible one.
+	/*
+	 * Check if the View is visible to the user, i.e on screen. We do this since
+	 * in tabbed interfaces, we can cause a scrollbar that's off screen to go to
+	 * the top as well as the visible one.
 	 */
 	private static boolean isViewInViewBounds(View mainView, View view) {
 		/* Failsafe */
